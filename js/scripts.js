2684ebdb161b3b9f899d78256117e0ce5429de9d
nkhakwan <post_khan@yahoo.com>
2020-03-24 11:44:35 -0700

    practiced jquery basics along with addinga and removing classes

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle();
   });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
