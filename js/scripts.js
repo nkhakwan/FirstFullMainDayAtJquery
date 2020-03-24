bbfc9a311ee4c223cf7552e16c5791408cc31fc2
nkhakwan <post_khan@yahoo.com>
2020-03-24 16:25:41 -0700

    everything completed. Need to go over again for some sanity

--- js/scripts.js
+++ js/scripts.js
@@ -15,27 +15,26 @@ $(document).ready(function(){
     $("body").removeClass();
     $("body").addClass("red-background");
 
-    $("button#hello").click(function() {
-      $("ul#user").prepend("<li>Hello!</li>");
-      $("ul#webPage").prepend("<li>why Hello!</li>");
-      $('li').css('background-color', 'green');
-      $("ul#user").children("li").first().click(function() {
-        alert('hi');
-      });
-      $("ul#user").children("li").first().click(function() {
-        $(this).remove();
-      });
-   
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webPage").prepend("<li>why Hello!</li>");
+    $('li').css('background-color', 'green');
+    $("ul#user").children("li").first().click(function() {
+      alert('hi');
     });
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
   
-    $("button#goodbye").click(function() {
-      $("ul#user").prepend("<li>Goodbye!</li>");
-      $("ul#webPage").prepend("<li>llo!</li>");
-    });
-  
-    $("button#stop").click(function() {
-      $("ul#user").prepend("<li>Stop copying me!</li>");
-      $("ul#webPage").prepend("<li>lo!</li>");
-    });
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webPage").prepend("<li>llo!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webPage").prepend("<li>lo!</li>");
+  });
   });
 });
---
+++ js/sscripts.js
@@ -0,0 +1,47 @@
+//The first block would append the input to first inserted input. Won't delete the
+// first input. Second would, instead of appending add the
+//input afresh.
+
+/*$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});*/
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
