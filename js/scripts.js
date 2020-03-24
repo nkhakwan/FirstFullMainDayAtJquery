f6c276626f290ed6010216c3d4508016645c26b7
nkhakwan <post_khan@yahoo.com>
2020-03-24 11:07:27 -0700

    simple jQuery effects practiced and added

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,6 @@
-
-
 $(document).ready(function(){
-
-
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("This is an image.");
+  $(".clickabl").click(function(){
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
   });
 });
